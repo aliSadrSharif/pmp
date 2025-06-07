8b8ac86663fe3e81a364749b3e98273fefa2ea86
Ali Sadrsharif <alisadrsharif@gmail.com>
2025-06-07 15:39:33 +0330

    login page console log

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,19 +1,25 @@
-import { Button, Input, Form } from "@heroui/react";
-import PasswordInput from "@/components/passwordInput";
+"use client";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Form } from "@heroui/form";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
 
-export default function Login() {
+export default function Component() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const name = formData.get("name");
     const password = formData.get("password");
-    console.log(name);
-    console.log(password);
+    console.log(name, password);
   };
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6 border-2 border-purple-800 border-dashed shadow-2xl">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6 border-2 border-dashed border-purple-700">
         <p className="pb-4 text-left text-3xl font-semibold">
           Log In
           <span aria-label="emoji" className="ml-2" role="img">
@@ -34,8 +40,31 @@ export default function Login() {
             type="name"
             variant="bordered"
           />
-          <PasswordInput />
 
+          <Input
+            isRequired
+            endContent={
+              <button type="button" onClick={toggleVisibility}>
+                {isVisible ? (
+                  <Icon
+                    className="pointer-events-none text-2xl text-default-400"
+                    icon="solar:eye-closed-linear"
+                  />
+                ) : (
+                  <Icon
+                    className="pointer-events-none text-2xl text-default-400"
+                    icon="solar:eye-bold"
+                  />
+                )}
+              </button>
+            }
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="Enter your password"
+            type={isVisible ? "text" : "password"}
+            variant="bordered"
+          />
           <Button className="w-full mt-10" color="primary" type="submit">
             Log In
           </Button>
