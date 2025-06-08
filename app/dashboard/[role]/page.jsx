173aac27462dd12759963358f6c038b4adae5d61
Ali Sadrsharif <alisadrsharif@gmail.com>
2025-06-08 18:25:51 +0330

    adding hero to sign out

--- app/dashboard/[role]/page.jsx
+++ app/dashboard/[role]/page.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@heroui/button";
+import { Form } from "@heroui/form";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Dashboard() {
-  const cookieStore = cookies();
+export default async function Dashboard() {
+  const cookieStore = await cookies();
   const foundUser = cookieStore.get("foundUser")?.value;
 
   const user = foundUser ? JSON.parse(foundUser) : null;
@@ -10,25 +12,26 @@ export default function Dashboard() {
   console.log("User on dashboard page:", user);
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1>Test Page</h1>
-      {user ? (
-        <div>
-          <p>Welcome, {user.name}!</p>
-          <p>Your role: {user.role}</p>
-          {/* Logout button */}
-          <form action={handleLogout}>
-            <button
-              type="submit"
-              className="mt-4 px-4 py-2 bg-red-600 text-white rounded"
-            >
-              Logout
-            </button>
-          </form>
-        </div>
-      ) : (
-        <p>No user data found.</p>
-      )}
+    <div className="h-screen bg-gradient-to-tr from-blue-300 to-violet-300">
+      <div className="flex flex-col items-center gap-4">
+        <h1>Dashboard Page</h1>
+        {user ? (
+          <div>
+            <p>Welcome, {user.name}!</p>
+            <p>Your role: {user.role}</p>
+            <Form action={handleLogout}>
+              <Button
+                type="submit"
+                className="mt-4 px-4 py-2 bg-red-600 text-white rounded"
+              >
+                Logout
+              </Button>
+            </Form>
+          </div>
+        ) : (
+          <p>No user data found.</p>
+        )}
+      </div>
     </div>
   );
 }
