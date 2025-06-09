dc2c7a2f4ec1be403b1ddfc734614ee122860842
Ali Sadrsharif <alisadrsharif@gmail.com>
2025-06-09 20:41:20 +0330

    styling dashboard main task part 1

--- app/dashboard/[role]/page.jsx
+++ app/dashboard/[role]/page.jsx
@@ -4,6 +4,7 @@ import Sidebar from "@/components/Sidebar";
 import { handleLogout } from "@/lib/handleLogout";
 import { Button } from "@heroui/button";
 import { useUserFromCookie } from "@/lib/useUserFromCookie";
+import MainTaskbar from "@/components/MainTaskbar";
 
 export default function Dashboard() {
   const user = useUserFromCookie();
@@ -14,21 +15,26 @@ export default function Dashboard() {
     <div>
       {user ? (
         <div className="flex items-start">
-          <div className="flex items-center justify-between mt-10 w-9/12">
-            <p className="font-bold ml-10">
-              <span className="text-orange-800">{user.name}</span> as{" "}
-              {user.role}
-            </p>
-            <form action={handleLogout} className="mr-15">
-              <Button
-                type="submit"
-                className="mt-4 px-4 py-2 bg-red-600 text-white rounded"
-              >
-                Logout
-              </Button>
-            </form>
+          <div className="flex flex-col mt-10 w-full">
+            <div className="flex items-center justify-between w-full">
+              <p className="font-bold ml-10">
+                <span className="text-orange-800">{user.name}</span> as{" "}
+                {user.role}
+              </p>
+              <form action={handleLogout} className="mr-15">
+                <Button
+                  type="submit"
+                  className="mt-4 px-4 py-2 bg-red-600 text-white rounded"
+                >
+                  Logout
+                </Button>
+              </form>
+            </div>
+            <div className="w-full bg-pink-400 mt-10 h-[calc(100vh-136px)]">
+              <MainTaskbar />
+            </div>
           </div>
-          <div className="h-screen bg-indigo-500 w-3/12 flex grow-0">
+          <div className="h-screen bg-indigo-500 w-3/12 flex shrink-0 max-w-[15rem]">
             <Sidebar />
           </div>
         </div>
